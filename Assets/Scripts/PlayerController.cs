de69a7028e51405411f485f4232463898518efee
1210Purav <pshah6632@gmail.com>
2026-06-11 13:21:40 +0530

    Player keeps within Camera
    using Viewport method added boundaries around the camera to avoid player from going out of the FOV.
    
    PS: The lecture to add this functionality was very irritating.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,19 @@ public class PlayerController : MonoBehaviour
     InputAction moveAction;
 
     Vector3 moveVector;
+    Vector2 minBounds;
+    Vector2 maxBounds;
     [SerializeField] float moveSpeed = 10;
+    [SerializeField] float LeftBoundPadding ;
+    [SerializeField] float RightBoundPadding ;
+    [SerializeField] float UpBoundPadding ;
+    [SerializeField] float DownBoundPadding ;
+
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        InitBounds();
     }
 
     
@@ -17,9 +25,18 @@ public class PlayerController : MonoBehaviour
     {
         movePlayer();
     }
+    void InitBounds()
+    {
+        Camera mainCam = Camera.main;
+        minBounds = mainCam.ViewportToWorldPoint(new Vector2(0,0));
+        maxBounds = mainCam.ViewportToWorldPoint(new Vector2(1,1));
+    }
     void movePlayer()
     {
-        moveVector = moveAction.ReadValue<Vector2>() ;
-        transform.position += moveVector * moveSpeed * Time.deltaTime; //if moveVector is set to Vector2 it causes an error.
+        moveVector = moveAction.ReadValue<Vector2>();
+        Vector3 newPos = transform.position + moveVector * moveSpeed * Time.deltaTime; //if moveVector is set to Vector2 it causes an error.
+        newPos.x = Mathf.Clamp(newPos.x, minBounds.x + LeftBoundPadding, maxBounds.x - RightBoundPadding);
+        newPos.y = Mathf.Clamp(newPos.y, minBounds.y + DownBoundPadding, maxBounds.y - UpBoundPadding);
+        transform.position = newPos; 
     }
 }
