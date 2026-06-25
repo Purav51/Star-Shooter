2417eb3ec4ed6b735eea463c076189f7fc54a854
1210Purav <pshah6632@gmail.com>
2026-06-25 12:06:46 +0530

    Repeating waves indefinately
    Used a do while loop to make the waves repeat indefinately.

--- Assets/Scripts/enemySpawner.cs
+++ Assets/Scripts/enemySpawner.cs
@@ -7,28 +7,33 @@ public class enemySpawner : MonoBehaviour
     [SerializeField] WaveConfigSO currentWave;
     [SerializeField] float timeBetweenWaves = 1f;
     [SerializeField] WaveConfigSO[] WaveConfigs;
+    [SerializeField] bool isLooping;
+
     void Start()
     {
         StartCoroutine(SpawnEnemies());
     }
     IEnumerator SpawnEnemies()
     {
-        foreach (WaveConfigSO wave in WaveConfigs)
+        do
         {
-            currentWave = wave;
-            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            foreach (WaveConfigSO wave in WaveConfigs)
             {
-                Instantiate(currentWave.GetenemyPrefabIndex(i),
-                currentWave.GetStartingWayPoints().position,
-                Quaternion.identity,
-                transform);
+                currentWave = wave;
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate(currentWave.GetenemyPrefabIndex(i),
+                    currentWave.GetStartingWayPoints().position,
+                    Quaternion.identity,
+                    transform);
 
-                yield return new WaitForSeconds(currentWave.GetRandEnemySpawnTime());
+                    yield return new WaitForSeconds(currentWave.GetRandEnemySpawnTime());
+                }
+                // wave ends
+                yield return new WaitForSeconds(timeBetweenWaves); // wait before the start of next wave. 
             }
-            // wave ends
-            yield return new WaitForSeconds(timeBetweenWaves); // wait before the start of next wave. 
         }
-
+        while (isLooping);
     }
     public WaveConfigSO GetCurrentWave()
     {
