deee117a2da6d7a3c3941ac0f25825bd17fbbd63
1210Purav <pshah6632@gmail.com>
2026-07-01 17:00:19 +0530

    Music Stays consistent

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,12 +4,34 @@ public class AudioManager : MonoBehaviour
 {
     [Header("Shooting SFX")]
     [SerializeField] AudioClip shootingClip;
-    [SerializeField] [Range(0,1)] float ShootingVolume;
+    [SerializeField][Range(0, 1)] float ShootingVolume;
 
     [Header("Damage SFX")]
     [SerializeField] AudioClip damageClip;
-    [SerializeField] [Range(0,1)] float DamageVolume;
+    [SerializeField][Range(0, 1)] float DamageVolume;
 
+    static AudioManager instance;
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+    void ManageSingleton()
+    {
+        // int instanceCount = FindObjectsByType<AudioManager>(FindObjectsSortMode.None).Length;
+        // if(instanceCount > 1)
+
+        if (instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
     public void PlayShootingSFX()
     {
         if (shootingClip != null)
@@ -27,7 +49,7 @@ public class AudioManager : MonoBehaviour
 
     private void PlayAudioClip(AudioClip clip, float volume)
     {
-        if(clip != null)
+        if (clip != null)
         {
             AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position, volume);
         }
